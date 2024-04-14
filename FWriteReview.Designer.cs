77fc0182e81f0f8e0a4313442917443d5dd36aa6
Nguyen Hieu Nghia <hieunghia484@gmail.com>
2024-04-14 12:59:12 +0700

    sua giao dien danh gia

--- FWriteReview.Designer.cs
+++ FWriteReview.Designer.cs
@@ -29,21 +29,24 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblAddimage = new System.Windows.Forms.LinkLabel();
             this.btnSend = new Guna.UI2.WinForms.Guna2Button();
             this.txtdetail = new Guna.UI2.WinForms.Guna2TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.guna2RatingStar1 = new Guna.UI2.WinForms.Guna2RatingStar();
             this.ptReview = new Guna.UI2.WinForms.Guna2PictureBox();
             this.tbtJobName = new Guna.UI2.WinForms.Guna2TextBox();
-            this.lblAddimage = new System.Windows.Forms.LinkLabel();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ptReview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.lblAddimage);
+            this.panel1.Controls.Add(this.guna2PictureBox1);
             this.panel1.Controls.Add(this.btnSend);
             this.panel1.Controls.Add(this.txtdetail);
             this.panel1.Controls.Add(this.label1);
@@ -53,10 +56,21 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1121, 535);
+            this.panel1.Size = new System.Drawing.Size(766, 694);
             this.panel1.TabIndex = 5;
+            // 
+            // lblAddimage
+            // 
+            this.lblAddimage.AutoSize = true;
+            this.lblAddimage.BackColor = System.Drawing.Color.LightGray;
+            this.lblAddimage.Location = new System.Drawing.Point(107, 539);
+            this.lblAddimage.Name = "lblAddimage";
+            this.lblAddimage.Size = new System.Drawing.Size(65, 24);
+            this.lblAddimage.TabIndex = 6;
+            this.lblAddimage.TabStop = true;
+            this.lblAddimage.Text = "image";
             // 
             // btnSend
             // 
@@ -67,10 +81,10 @@
             this.btnSend.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(41)))), ((int)(((byte)(123)))));
             this.btnSend.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSend.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(159)))), ((int)(((byte)(1)))));
-            this.btnSend.Location = new System.Drawing.Point(463, 471);
-            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSend.Location = new System.Drawing.Point(69, 620);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(172, 51);
+            this.btnSend.Size = new System.Drawing.Size(647, 51);
             this.btnSend.TabIndex = 5;
             this.btnSend.Text = "SEND";
             // 
@@ -89,20 +103,20 @@
             this.txtdetail.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtdetail.ForeColor = System.Drawing.Color.Black;
             this.txtdetail.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtdetail.Location = new System.Drawing.Point(41, 221);
+            this.txtdetail.Location = new System.Drawing.Point(69, 244);
             this.txtdetail.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.txtdetail.Name = "txtdetail";
             this.txtdetail.PasswordChar = '\0';
             this.txtdetail.PlaceholderText = "";
             this.txtdetail.SelectedText = "";
-            this.txtdetail.Size = new System.Drawing.Size(1035, 240);
+            this.txtdetail.Size = new System.Drawing.Size(647, 240);
             this.txtdetail.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(70, 186);
+            this.label1.Location = new System.Drawing.Point(284, 196);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(223, 29);
@@ -111,10 +125,10 @@
             // 
             // guna2RatingStar1
             // 
-            this.guna2RatingStar1.Location = new System.Drawing.Point(659, 13);
-            this.guna2RatingStar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2RatingStar1.Location = new System.Drawing.Point(232, 81);
+            this.guna2RatingStar1.Margin = new System.Windows.Forms.Padding(4);
             this.guna2RatingStar1.Name = "guna2RatingStar1";
-            this.guna2RatingStar1.Size = new System.Drawing.Size(251, 36);
+            this.guna2RatingStar1.Size = new System.Drawing.Size(329, 57);
             this.guna2RatingStar1.TabIndex = 2;
             // 
             // ptReview
@@ -122,8 +136,8 @@
             this.ptReview.BorderRadius = 20;
             this.ptReview.FillColor = System.Drawing.Color.Gainsboro;
             this.ptReview.ImageRotate = 0F;
-            this.ptReview.Location = new System.Drawing.Point(905, 69);
-            this.ptReview.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ptReview.Location = new System.Drawing.Point(13, 0);
+            this.ptReview.Margin = new System.Windows.Forms.Padding(4);
             this.ptReview.Name = "ptReview";
             this.ptReview.Size = new System.Drawing.Size(150, 106);
             this.ptReview.TabIndex = 1;
@@ -144,39 +158,44 @@
             this.tbtJobName.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbtJobName.ForeColor = System.Drawing.Color.Black;
             this.tbtJobName.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.tbtJobName.Location = new System.Drawing.Point(41, 6);
+            this.tbtJobName.Location = new System.Drawing.Point(190, 6);
             this.tbtJobName.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.tbtJobName.Name = "tbtJobName";
             this.tbtJobName.PasswordChar = '\0';
             this.tbtJobName.PlaceholderText = "";
             this.tbtJobName.SelectedText = "";
-            this.tbtJobName.Size = new System.Drawing.Size(491, 55);
+            this.tbtJobName.Size = new System.Drawing.Size(371, 55);
             this.tbtJobName.TabIndex = 0;
             // 
-            // lblAddimage
+            // guna2PictureBox1
             // 
-            this.lblAddimage.AutoSize = true;
-            this.lblAddimage.Location = new System.Drawing.Point(818, 109);
-            this.lblAddimage.Name = "lblAddimage";
-            this.lblAddimage.Size = new System.Drawing.Size(65, 24);
-            this.lblAddimage.TabIndex = 6;
-            this.lblAddimage.TabStop = true;
-            this.lblAddimage.Text = "image";
+            this.guna2PictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox1.BorderRadius = 20;
+            this.guna2PictureBox1.FillColor = System.Drawing.Color.Gainsboro;
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(69, 494);
+            this.guna2PictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(150, 106);
+            this.guna2PictureBox1.TabIndex = 7;
+            this.guna2PictureBox1.TabStop = false;
+            this.guna2PictureBox1.Click += new System.EventHandler(this.guna2PictureBox1_Click);
             // 
             // FWriteReview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1121, 535);
+            this.ClientSize = new System.Drawing.Size(766, 694);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FWriteReview";
             this.Text = "FWriteReview";
             this.Load += new System.EventHandler(this.FWriteReview_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ptReview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -191,5 +210,6 @@
         private Guna.UI2.WinForms.Guna2PictureBox ptReview;
         private Guna.UI2.WinForms.Guna2TextBox tbtJobName;
         private System.Windows.Forms.LinkLabel lblAddimage;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
     }
 }
--- FWriteReview.cs
+++ FWriteReview.cs
@@ -79,5 +79,10 @@ namespace TheGioiViecLam
                 conn.Close();
             }
         }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
